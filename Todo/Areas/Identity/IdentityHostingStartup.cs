e3223502507497be4f0c64b3b042ce511a2b4381
joshdefreitas <joshdefreitas@gmail.com>
2021-09-24 21:02:39 -0700

    dotnet core with scaffolded identity

---
+++ Todo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Areas.Identity.Data;
+using Todo.Data;
+
+[assembly: HostingStartup(typeof(Todo.Areas.Identity.IdentityHostingStartup))]
+namespace Todo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TodoDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TodoDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TodoDBContext>();
+            });
+        }
+    }
+}
